b020c22643422c6019d607ad0a5ba0ecb7002e17
Oghenevwegba Obire <jesseoghenevwegba@outlook.com>
2023-04-10 18:35:23 +0100

    10-4-2023

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 "use strict";
 
+let currentScore = 0;
+let activePlayer = 0;
+
 const score0Element = document.getElementById("score--0");
 const score1Element = document.getElementById("score--1");
 const current0Element = document.getElementById("current--0");
@@ -10,7 +13,12 @@ const dice = document.getElementsByClassName("dice")[0];
 const newGameButton = document.getElementsByClassName("btn--new")[0];
 const rollButton = document.getElementsByClassName("btn--roll")[0];
 const holdButton = document.getElementsByClassName("btn--hold")[0];
+const scores = [0, 0];
 const restart = function () {
+    scores[0] = 0;
+    scores[1] = 0;
+    currentScore = 0;
+    activePlayer = 0;
     score0Element.textContent = 0;
     score1Element.textContent = 0;
     current0Element.textContent = 0;
@@ -60,45 +68,56 @@ const rollFunction = function () {
         let number = rollDice();
         if (document.getElementsByClassName("player--active")[0].classList.contains("player--0")) {
             if (number !== 1) {
-                current0Element.textContent = Number(current0Element.textContent) + number;
+                currentScore += number;
+                current0Element.textContent = currentScore;
             } else {
+                currentScore = 0;
                 current0Element.textContent = 0;
                 player0Element.classList.remove("player--active");
                 player1Element.classList.add("player--active");
+                activePlayer = 1;
             }
         } else {
             if (number !== 1) {
-                current1Element.textContent = Number(current1Element.textContent) + number;
+                currentScore += number;
+                current1Element.textContent = currentScore;
             } else {
+                currentScore = 0;
                 current1Element.textContent = 0;
                 player1Element.classList.remove("player--active");
                 player0Element.classList.add("player--active");
+                activePlayer = 0;
             }
         }
     }
 };
 const holdFunction = function () {
     if (document.getElementsByClassName("player--active")[0].classList.contains("player--0")) {
-        score0Element.textContent = Number(score0Element.textContent) + Number(current0Element.textContent);
+        scores[0] += currentScore;
+        score0Element.textContent = scores[0];
         current0Element.textContent = 0;
         if (Number(score0Element.textContent) < 100) {
             player0Element.classList.remove("player--active");
             player1Element.classList.add("player--active");
+            activePlayer = 1;
         } else {
             player0Element.classList.add("player--winner");
             gameOver = true;
         }
     } else {
-        score1Element.textContent = Number(score1Element.textContent) + Number(current1Element.textContent);
+        scores[1] += currentScore;
+        score1Element.textContent = scores[1];
         current1Element.textContent = 0;
         if (Number(score1Element.textContent) < 100) {
             player1Element.classList.remove("player--active");
             player0Element.classList.add("player--active");
+            activePlayer = 0;
         } else {
             player1Element.classList.add("player--winner");
             gameOver = true;
         }
     }
+    currentScore = 0;
 };
 let gameOver = false;
 restart();
